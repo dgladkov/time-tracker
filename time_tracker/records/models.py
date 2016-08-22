6db4b36bce18a7a50169eed53ec51dd3c509fbe4
tsh <dr.tallin@gmail.com>
2016-08-22 17:30:38 +0300

    add records model

--- time_tracker/records/models.py
+++ time_tracker/records/models.py
@@ -1,3 +1,17 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.TextField(blank=True)
+    # owner = company/user ?
+    
+    
+class Record(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    project = models.ForeignKey(Project, null=True, blank=True)
+    time_spent = models.DecimalField(max_digits=5, decimal_places=2)
+    date = models.DateTimeField()
+    description = models.TextField(blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
--- time_tracker/time_tracker/settings.py
+++ time_tracker/time_tracker/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework_swagger',
     # apps
     'registration',
+    'records',
 ]
 
 MIDDLEWARE_CLASSES = [
