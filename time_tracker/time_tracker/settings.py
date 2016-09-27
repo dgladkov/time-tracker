90caf333d846b84a5df03f452f267522868f746d
tsh <dr.tallin@gmail.com>
2016-09-27 16:20:44 +0300

    change db

--- time_tracker/time_tracker/settings.py
+++ time_tracker/time_tracker/settings.py
@@ -83,8 +83,13 @@ WSGI_APPLICATION = 'time_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'time_tracker',
+        'USER': 'uwsgi',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+
     }
 }
 
