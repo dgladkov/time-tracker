a1a9f8d295936fc8c57be9663ef6168719d0b952
tsh <dr.tallin@gmail.com>
2016-09-27 15:06:59 +0300

    add session auth

--- time_tracker/time_tracker/settings.py
+++ time_tracker/time_tracker/settings.py
@@ -132,6 +132,7 @@ AUTH_USER_MODEL = 'registration.User'
 REST_FRAMEWORK = {
     # Auth
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
 }
