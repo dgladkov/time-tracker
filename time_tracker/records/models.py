9cf4a6a6dea97321b2efa27f5bb11fffec8e6cb1
tsh <dr.tallin@gmail.com>
2016-09-27 15:12:59 +0300

    change date field

--- time_tracker/records/models.py
+++ time_tracker/records/models.py
@@ -12,6 +12,6 @@ class Record(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
     project = models.ForeignKey(Project, null=True, blank=True)
     time_spent = models.DecimalField(max_digits=5, decimal_places=2)
-    date = models.DateTimeField()
+    date = models.DateField()
     description = models.TextField(blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
